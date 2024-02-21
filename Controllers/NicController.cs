5e61b97d22b84e258aab1a581c2873cd8aca59aa
Abhisheka(AbhishekaP <Abhisheka(AbhishekaP@ENADOC-END-02>
2024-02-21 15:48:58 +0530

    Add project files.

---
+++ Controllers/NicController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NicController : ControllerBase
+    {
+        private readonly IConfiguration config;
+        public NicController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpGet("createUser")]
+        public IActionResult Create()
+        {
+            return Ok("success from Create Method");
+        }
+    }
+}
---
+++ Models/NicDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class NicDetails
+    {
+        public int NicNumber  { get; set; }
+       
+    }
+}
